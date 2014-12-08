64ea2aab3e2da4bab5e4613f3724b39022453817
ShinnosukeMunakata <shinnosuke.munakata@gmail.com>
2014-12-08 12:45:49 +0900

    @CPlayer 弾を発射できるように（Spaceキー） プレイヤーの移動がまだうまく出来ません
    @CBullet
    各関数を作成
    
    @Billboard
    _Size -> protectedに

--- GLFrameWork/Game.cpp
+++ GLFrameWork/Game.cpp
@@ -14,10 +14,6 @@
 const float CGame::RADIUS_SKY = 500.0f;   // 空の半径
 #include "CPlayer.h"
 
-CPlayer* g_player;
-CModel* g_model;
-
-
 CGame::CGame()
 {
 
@@ -29,40 +25,22 @@ CGame::~CGame()
 
 void CGame::Init(void)
 {
-  // 地形生成
-  Ground = nullptr;
-  Ground = CMeshGround::Create(VECTOR3(0.0f, 0.0f, 0.0f), VECTOR2(50.0f, 50.0f), VECTOR2(20.0f, 20.0f));
-  Ground->SetTex(CTexture::Texture(TEX_FIELD));
-
-  // 空生成
-  Sky = nullptr;
-  Sky = CMeshSphere::Create(VECTOR3(0.0f, 0.0f, 0.0f), VECTOR2(16.0f, 8.0f), RADIUS_SKY);
-  Sky->SetTex(CTexture::Texture(TEX_MIKU));
-
-  // プレイヤー生成
-  Player = CModel::Create(CModel::MIKU, VECTOR3(0.0f, 0.0f, 0.0f));
-  Player->SetTex(CTexture::Texture(TEX_MIKU));
-  Player->SetScl(20.0f, 20.0f, 20.0f);
-	
-	//CPolygon3D* polygon = CPolygon3D::Create(VECTOR3(-200.0f,0,0),VECTOR2(250.0f,250.0f),VECTOR3(0,0,90.0f));
-	//polygon->SetTex(CTexture::Texture(TEX_LIGHT));
-	//polygon->SetColor(GREEN(1.0f));
-	CPolygon3D::Create(VECTOR3(0,-100.0f,0),VECTOR2(500.0f,500.0f),VECTOR3(0.0f,0,0));
-
-	// 2014-12-01
-	// 宗像：テスト
-	g_player = CPlayer::Create(CModel::MIKU, VECTOR3(0.0f, 0.0f, 0.0f));
-	g_player->SetTex(CTexture::Texture(TEX_MIKU));
-	g_player->SetScl(VECTOR3(60.0f, 60.0f, 60.0f));
-	//g_player->SetRotY(180.0f);
-	
+	// 地形生成
+	Ground = nullptr;
 	/*
-	g_model = CModel::Create(CModel::MIKU, VECTOR3(0.0f, 0.0f, 0.0f));
-	//g_model->SetTex(CTexture::Texture(TEX_MIKU));
-	g_model->SetScl(VECTOR3(60.0f, 60.0f, 60.0f));
-	g_model->SetRotY(180.0f);
+	Ground = CMeshGround::Create(VECTOR3(0.0f, 0.0f, 0.0f), VECTOR2(50.0f, 50.0f), VECTOR2(20.0f, 20.0f));
+	Ground->SetTex(CTexture::Texture(TEX_FIELD));
 	*/
 
+	// 空生成
+	Sky = nullptr;
+	Sky = CMeshSphere::Create(VECTOR3(0.0f, 0.0f, 0.0f), VECTOR2(16.0f, 8.0f), RADIUS_SKY);
+	Sky->SetTex(CTexture::Texture(TEX_MIKU));
+
+	// プレイヤー生成
+	Player = CPlayer::Create(CModel::MIKU, VECTOR3(0.0f, 0.0f, 0.0f));
+	Player->SetTex(CTexture::Texture(TEX_MIKU));
+	Player->SetScl(20.0f, 20.0f, 20.0f);
 }
 
 void CGame::Uninit(void)
@@ -93,28 +71,11 @@ void CGame::Uninit(void)
 
 void CGame::Update(void)
 {
-  // プレイヤー移動
-  if (CKeyboard::GetPress(DIK_W))
-  {
-    Player->AddPosZ(1.0f);
-  }
-  else if (CKeyboard::GetPress(DIK_S))
-  {
-    Player->AddPosZ(-1.0f);
-  }
-  if (CKeyboard::GetPress(DIK_A))
-  {
-    Player->AddPosX(1.0f);
-  }
-  else if (CKeyboard::GetPress(DIK_D))
-  {
-    Player->AddPosX(-1.0f);
-  }
-
+  // TODO：以下一行コメントアウトは作業しにくいから。終了し次第消します
   // 地形とのあたり判定
   VECTOR3 NormalGround;   // 地形の法線
-  float   HeightGround;   // 地形の高さ
-  HeightGround = Ground->GetHeight(Player->Pos(), &NormalGround);
+  //float   HeightGround;   // 地形の高さ
+  //HeightGround = Ground->GetHeight(Player->Pos(), &NormalGround);
   NormalGround.Normalize();
 
   // 回転を求める
@@ -137,8 +98,8 @@ void CGame::Update(void)
   AnglePlayerZ = VECTOR3::Dot(VectorNormalXY, VectorUpPlayer);
 
   // プレイヤーに設定する
-  Player->SetPosY(HeightGround);
-  Player->SetRot(180.0f / PI * AnglePlayerX, 0.0f, 180.0f / PI * AnglePlayerZ);
+  //Player->SetPosY(HeightGround);
+  //Player->SetRot(180.0f / PI * AnglePlayerX, 0.0f, 180.0f / PI * AnglePlayerZ);
 
 	if (CKeyboard::GetTrigger(DIK_RETURN))
 	{
