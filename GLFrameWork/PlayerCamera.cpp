57ab7772e9f6a55134b398ec770893356ebb327b
Arai <xeon_lagunas@yahoo.co.jp>
2015-02-16 11:39:00 +0900

    色々修正中

--- GLFrameWork/PlayerCamera.cpp
+++ GLFrameWork/PlayerCamera.cpp
@@ -10,7 +10,7 @@ CPlayerCamera* CPlayerCamera::Create(CObject* parent,float r)
 	CPlayerCamera* camera = new CPlayerCamera;
 	if (camera == nullptr){return nullptr;}
 
-	camera->Length = r;
+	camera->Length = r*5;
 	camera->Parent = parent;
 	camera->Init();
 
@@ -33,9 +33,9 @@ void CPlayerCamera::Init(void)
 
 	DestEye = _Eye;
 	VECTOR3 vec;
-	vec.x = sinf(rot.y);
+	vec.x = sinf(DEG2RAD(rot.y));
 	vec.y = 0;
-	vec.z = cosf(rot.y);
+	vec.z = cosf(DEG2RAD(rot.y));
 	vec.Normalize();
 	CSoundAL::SetListenerPos(_Eye);
 	CSoundAL::SetListenerFVec(vec);
@@ -56,15 +56,15 @@ void CPlayerCamera::Update(void)
 
 	_Eye.y += (DestEye.y - _Eye.y)*0.5f;
 
-	_Lookat.x = sinf(DEG2RAD(rot.y))*Length;
+	_Lookat.x = sinf(DEG2RAD(rot.y)+PI/2)*Length;
 	_Lookat.y = 0;
-	_Lookat.z = cosf(DEG2RAD(rot.y))*Length;
+	_Lookat.z = cosf(DEG2RAD(rot.y) + PI / 2)*Length;
 	_Lookat += pos;
 
 	VECTOR3 vec;
-	vec.x = sinf(rot.y);
+	vec.x = sinf(DEG2RAD(rot.y));
 	vec.y = 0;
-	vec.z = cosf(rot.y);
+	vec.z = cosf(DEG2RAD(rot.y));
 	vec.Normalize();
 
 	CSoundAL::SetListenerPos(_Eye);
